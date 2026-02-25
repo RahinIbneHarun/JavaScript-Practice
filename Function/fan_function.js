1fe9d3c724b2e6e948e1b01c69f51fc16b7f6fbb
RahinIbneHarun <arbin.rahin@gmail.com>
2026-02-25 20:03:43 +0600

    function part upload

---
+++ Function/fan_function.js
@@ -0,0 +1,6 @@
+function fanOffKor(){
+    console.log('bosha theke uthe dara');
+    console.log('walk toward the switch');
+    console.log('click the switch to off of the fan');
+}
+fanOffKor();
---
+++ Function/function_perameter1.js
@@ -0,0 +1,7 @@
+function doubleIt(number){
+    const double = number*8;
+    console.log(double);
+}
+doubleIt(15);
+doubleIt(7);
+doubleIt(40);
